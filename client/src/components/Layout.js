20c4c02ca9dd2a8917b53357e5597a08d73a58ff
file-a-case <filetransfer3444@gmail.com>
2025-05-17 15:24:05 +0530

    Name change

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -61,7 +61,7 @@ const Layout = ({ children }) => {
         <div className="layout">
           <div className="sidebar">
             <div className="logo">
-              <h6 className="text-light">DOC APP</h6>
+              <h6 className="text-light">MEDIBOOK</h6>
               <hr />
             </div>
             <div className="menu">
